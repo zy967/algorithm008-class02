726f9f82a2aee28711a013c51b102957af743cf8
zy967 <ys790094203@gmail.com>
2020-04-26 22:52:10 +0800

    Create isAnagram.py

---
+++ Week_02/isAnagram.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        dicts = collections.defaultdict(int)
+        for i in range(len(s)):
+            dicts[s[i]] = dicts[s[i]] + 1
+            dicts[t[i]] = dicts[t[i]] - 1
+        for val in dicts.values():
+            if val != 0:
+                return False
+        return True
